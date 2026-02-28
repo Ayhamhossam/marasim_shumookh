4f4e28b2f6cfc68c29073ff9da4c855fcc0ac086
Ayhamhossam <ayham776457566@gmail.com>
2026-02-28 06:55:40 +0300

    Refactor build.sh to streamline database updates
    Removed comments in Arabic and updated migration commands.

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# تثبيت المكتبات
 pip install -r requirements.txt
-
-# تجميع ملفات التنسيق
 python manage.py collectstatic --no-input
 
-# إنشاء ملفات الهجرة (التحديث)
+# تحديث قاعدة البيانات تلقائياً
 python manage.py makemigrations --noinput
-python manage.py makemigrations store --noinput
-
-# تطبيق التحديث على قاعدة البيانات (هذا هو السطر المنقذ)
 python manage.py migrate --noinput
