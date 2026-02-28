25048e5c812514c3e7f2cb2dbe3da2011a0464b5
Ayhamhossam <ayham776457566@gmail.com>
2026-02-28 06:25:54 +0300

    Refactor admin classes for Product and Sale
    Updated ProductAdmin and SaleAdmin classes to enhance display features and added profit calculations.

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
 from .models import Product, Sale
+from django.utils.timezone import now
+from django.db.models import Sum, F
+
+admin.site.site_header = "نظام مراسيم الشموخ للملابس"
 
 @admin.register(Product)
-class PAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cost', 'stock')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost_price', 'stock', 'display_sold_qty', 'display_profit')
+    search_fields = ('name',)
+    list_editable = ('stock',)
+
+    def display_sold_qty(self, obj): return obj.total_sold_qty()
+    display_sold_qty.short_description = "المباع"
+
+    def display_profit(self, obj): return f"{obj.total_product_profit()} ريال"
+    display_profit.short_description = "أرباح المنتج"
 
 @admin.register(Sale)
-class SAdmin(admin.ModelAdmin):
-    list_display = ('product', 'qty', 'sale_price', 'date')
-    list_filter = ('date',)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('product', 'qty', 'sale_price', 'get_profit', 'date', 'note')
+    list_filter = ('date', 'product')
+    date_hierarchy = 'date'
+    
+    def get_profit(self, obj):
+        return f"{obj.net_profit} ريال"
+    get_profit.short_description = "الربح الصافي"
+
+    def changelist_view(self, request, extra_context=None):
+        today = now().date()
+        today_sales = Sale.objects.filter(date__date=today)
+        extra_context = extra_context or {}
+        extra_context['today_count'] = today_sales.count()
+        extra_context['today_total'] = today_sales.aggregate(total=Sum(F('sale_price') * F('qty')))['total'] or 0
+        extra_context['today_profit'] = sum(s.net_profit for s in today_sales)
+        return super().changelist_view(request, extra_context=extra_context)
