c97a3b6714872b149b53d96d6b3b71b0e7c4c8f2
Ayhamhossam <ayham776457566@gmail.com>
2026-02-28 06:46:28 +0300

    Update build.sh for library installation and migrations
    Add commands for installing libraries and managing migrations.

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# تثبيت المكتبات
 pip install -r requirements.txt
+
+# تجميع ملفات التنسيق
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# إنشاء ملفات الهجرة (التحديث)
+python manage.py makemigrations --noinput
+python manage.py makemigrations store --noinput
+
+# تطبيق التحديث على قاعدة البيانات (هذا هو السطر المنقذ)
+python manage.py migrate --noinput
